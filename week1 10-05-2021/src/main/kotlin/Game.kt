4de134c08587ee94d284da1d4c1525adc554f442
kylian-2612 <71694721+kylian-2612@users.noreply.github.com>
2021-10-05 17:28:57 +0200

    solved rock, paper, scissors game

---
+++ week1 10-05-2021/src/main/kotlin/Game.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val options = arrayOf("rock", "paper", "scissors")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("computer picked $computerChoice")
+    println("user picked $userChoice")
+    determineWinner(computerChoice, userChoice)
+}
+
+fun getUserChoice(options: Array<String>):String?{
+    var userChoice: String?
+    do {
+        println("Make your choice: rock - paper - scissors")
+        userChoice = readLine()
+    } while (!options.contains(userChoice))
+
+    return  userChoice
+}
+
+fun determineWinner(computerChoice: String, userChoice:String?){
+    if(computerChoice == userChoice){
+        println("It's a draw")
+        return
+    }
+
+    val winMessage = "You've won"
+    val loseMessage = "You've lost"
+
+    when(computerChoice){
+        "rock" -> if(userChoice == "paper") println(winMessage) else println(loseMessage)
+        "paper" -> if(userChoice == "scissors") println(winMessage) else println(loseMessage)
+        "scissors" -> if(userChoice == "rock") println(winMessage) else println(loseMessage)
+    }
+}
