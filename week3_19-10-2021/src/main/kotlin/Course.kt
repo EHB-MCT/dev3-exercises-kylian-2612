92fef791be3441e552a9ea3b1d1e55bf4e47bb2a
kylian-2612 <71694721+kylian-2612@users.noreply.github.com>
2021-10-19 14:49:09 +0200

    created Course classes and .gitignore file

---
+++ week3_19-10-2021/src/main/kotlin/Course.kt
@@ -0,0 +1,10 @@
+class Course(
+    val title: String,
+    val teacher: String,
+    val description: String,
+    val semester: Int,
+    val track: String,
+    val credits: Int,
+    val year: Int,
+    ){
+}
---
+++ week3_19-10-2021/src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val Design = Course("Design 3", "Fenna", "creating concepts", 5, "Design", 5, 2)
+    Design.teacher
+}
+
